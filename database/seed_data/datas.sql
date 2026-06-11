570cf911aa08655335ecd9c25f563a7d771a0ed9
Shagana <shagana0211@gmail.com>
2026-06-11 23:05:50 +0530

    Added constraints and indexes

---
+++ database/seed_data/datas.sql
@@ -0,0 +1,308 @@
+/*[{
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "recruiter_id": "41c5e446-a128-4257-8707-1385299feafd",
+  "title": "HR Executive",
+  "description": "A Human Resources (HR) professional manages the employee lifecycle—from recruiting and onboarding to training, payroll, and conflict resolution. They align company policies with business goals and foster a positive workplace culture.A comprehensive, customizable job description template suitable for job boards or our organization",
+  "experience_required": 2,
+  "education_required": "MBA(HR)/BBA",
+  "status": "ACTIVE",
+  "created_at": "2026-06-11 13:32:50.040051",
+  "updated_at": "2026-06-11 13:32:50.040051"
+}, {
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "recruiter_id": "21a932de-acb0-4564-974f-e9b3e391eb57",
+  "title": "Talent Acquisition Specialist",
+  "description": "A Talent Acquisition Specialist is a strategic HR professional responsible for sourcing, vetting, and hiring top talent to meet a companys long-term business goals. They go beyond traditional hiring by building candidate pipelines, managing the recruitment cycle, and enhancing employer branding",
+  "experience_required": 2,
+  "education_required": "MBA HR / PGDM HR / Any Graduate with HR experience",
+  "status": "ACTIVE",
+  "created_at": "2026-06-11 13:32:50.040051",
+  "updated_at": "2026-06-11 13:32:50.040051"
+}]
+
+[{
+  "skill_id": "f4d8aef2-d63f-4eb5-ab8c-44b76c85ac56",
+  "skill_name": "Recruitment",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "6aabdc42-70d9-4bdf-a6da-b72bf91fb5b3",
+  "skill_name": "HR Policies",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "629d67d4-b690-46a9-a923-4aa38d49c966",
+  "skill_name": "Employee Relations",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "b5b47be0-d9db-40d8-901c-c02e8e5d46ed",
+  "skill_name": "Onboarding",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "f9cdbc4b-d300-49b7-a99d-cac35b87d42c",
+  "skill_name": "Payroll",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "3839f2c4-aec3-45e0-bc22-01f113c256b5",
+  "skill_name": "Performance Management",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "640e9a97-ba1b-47fc-8f8d-5ebc98fb24b8",
+  "skill_name": "HRMS Tools",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "cf09c9c3-0b23-4b24-80d3-1052584c23de",
+  "skill_name": "Talent Sourcing",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "17762ee4-ae4d-4121-a532-e9450202a972",
+  "skill_name": "LinkedIn Recruiting",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "653d490c-7b58-4797-858b-df843361e2a3",
+  "skill_name": "ATS Tools",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "bce08477-49cd-4fb6-8780-1dc93a06ddbe",
+  "skill_name": "Offer Management",
+  "category": "HR",
+  "created_at": "2026-06-11 13:16:47.152641"
+}, {
+  "skill_id": "1bc8895a-27c8-4b91-a680-dc21ebe531ac",
+  "skill_name": "Campus Hiring",
+  "category": "HR",
+  "created_at": "2026-06-11 13:44:04.840868"
+}, {
+  "skill_id": "23079dff-00f7-43f2-9e7c-ae574c6d9380",
+  "skill_name": "Headhunting",
+  "category": "HR",
+  "created_at": "2026-06-11 13:44:04.840868"
+}]
+[{
+  "jd_skill_id": "02296b3e-dd64-4022-a8c1-55cc851814b1",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "f4d8aef2-d63f-4eb5-ab8c-44b76c85ac56",
+  "priority_level": 1,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "a229d577-7fca-4fc2-a9e7-a3d45bb2a179",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "b5b47be0-d9db-40d8-901c-c02e8e5d46ed",
+  "priority_level": 2,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "9ce197e6-884d-4033-a756-b34260927fe4",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "6aabdc42-70d9-4bdf-a6da-b72bf91fb5b3",
+  "priority_level": 1,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "5c534269-1806-42f8-a26e-9314d1aa3e6c",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "629d67d4-b690-46a9-a923-4aa38d49c966",
+  "priority_level": 2,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "013a281a-7ece-4f2a-992c-9658b3d9daea",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "3839f2c4-aec3-45e0-bc22-01f113c256b5",
+  "priority_level": 2,
+  "mandatory_flag": false
+}, {
+  "jd_skill_id": "3b83097c-578e-4a83-b86b-193606e26353",
+  "jd_id": "a0c73d74-1a11-475b-a924-89a480d3a74e",
+  "skill_id": "640e9a97-ba1b-47fc-8f8d-5ebc98fb24b8",
+  "priority_level": 2,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "90d32eef-302a-4b86-8bad-99841ff9ad63",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "cf09c9c3-0b23-4b24-80d3-1052584c23de",
+  "priority_level": 1,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "cf85419b-c1f6-40d6-983c-3fdc674ea670",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "17762ee4-ae4d-4121-a532-e9450202a972",
+  "priority_level": 1,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "c0d612c6-98ec-42c2-bc70-6d00ecd17384",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "f4d8aef2-d63f-4eb5-ab8c-44b76c85ac56",
+  "priority_level": 1,
+  "mandatory_flag": true
+}, {
+  "jd_skill_id": "9ca9ebe1-de2e-4533-ab8c-9678c6aacd6f",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "bce08477-49cd-4fb6-8780-1dc93a06ddbe",
+  "priority_level": 1,
+  "mandatory_flag": false
+}, {
+  "jd_skill_id": "21ecf791-b528-443a-b115-966013e6972c",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "1bc8895a-27c8-4b91-a680-dc21ebe531ac",
+  "priority_level": 2,
+  "mandatory_flag": false
+}, {
+  "jd_skill_id": "ca5d1986-b43e-46cb-809c-bc90752423b1",
+  "jd_id": "957b0857-c274-42f6-a3e0-ef52e8966163",
+  "skill_id": "23079dff-00f7-43f2-9e7c-ae574c6d9380",
+  "priority_level": 3,
+  "mandatory_flag": false
+}]
+
+[{
+  "user_id": "a40334c8-975a-49a6-8d11-ead4b7429cf7",
+  "first_name": "Gowtham",
+  "email": "sanjaykk6050@gmail.com",
+  "password_hash": "scrypt:32768:8:1$K9EvD8S8WWrqm4vG$e2f741b22a777c18fcf413f1f860c549333b505ed7397d355d1c2da49bccc842e1885d4b840ef581decb7dbfbf1eec62b5ba951a2987b7d0fc41ce708bfacefa",
+  "role": "RECRUITER",
+  "department": "IT Hiring",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-11 13:30:20.183562",
+  "updated_at": "2026-06-11 13:30:20.183568",
+  "last_name": "N",
+  "phone_number": "9638236565"
+}, {
+  "user_id": "4fc34ce4-f3b4-4c4b-b9d4-5917aefac4a1",
+  "first_name": "Abhay",
+  "email": "hireai.team@gmail.com",
+  "password_hash": "$2b$12$zAQIxZpNjkJ1e84Jqo5UbeKviXZyPCyZ244EQj0E6/9pA8uTD3aVu",
+  "role": "ADMIN",
+  "department": "Management",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": "2026-06-11 07:02:47.070155",
+  "created_at": "2026-06-11 06:40:22.591611",
+  "updated_at": "2026-06-11 07:02:24.123501",
+  "last_name": "Krishnan",
+  "phone_number": "8825963034"
+}, {
+  "user_id": "910abcb2-2e95-40ac-a064-145b97e451c7",
+  "first_name": "Lilly",
+  "email": "elamathyramanan15@gmail.com",
+  "password_hash": "scrypt:32768:8:1$RwKNhcjJ0s80lVdu$f98f09cad55568cd1c363628f02f074651dae9564b5ebc55b8ec29eaefe785151d019285bfcbacbf53582f4031a825485f22ad491e39ae55991797806daaaf81",
+  "role": "RECRUITER",
+  "department": "Sales",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-11 15:36:26.320107",
+  "updated_at": "2026-06-11 15:36:26.320121",
+  "last_name": "E",
+  "phone_number": "9087654321"
+}, {
+  "user_id": "f1c7389f-37aa-4175-beb2-2f1022836d19",
+  "first_name": "Nandhini",
+  "email": "vnandhini658@gmail.com",
+  "password_hash": "nandhini123",
+  "role": "RECRUITER",
+  "department": "IT Hiring",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-05 14:10:11.731031",
+  "updated_at": "2026-06-10 20:28:19.23383",
+  "last_name": "",
+  "phone_number": "8807969637"
+}, {
+  "user_id": "48b00686-4c57-4ae4-8966-e187258f6e79",
+  "first_name": "Sanjay",
+  "email": "kksanjay2410@gmail.com",
+  "password_hash": "$2b$12$4yFGku3fiddlNxLIsCFAKeAMtTO8k6Q3ZtHfOdpYfQzKw0z0spDji",
+  "role": "ADMIN",
+  "department": "Management",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": "2026-06-11 17:29:15.129892",
+  "created_at": "2026-06-05 14:10:11.731031",
+  "updated_at": "2026-06-11 17:28:09.019627",
+  "last_name": "K K",
+  "phone_number": "7373684151"
+}, {
+  "user_id": "f14e272d-62d7-47f0-b4df-d47ee59fbabd",
+  "first_name": "Vikram ",
+  "email": "nilaela56@gmail.com",
+  "password_hash": "scrypt:32768:8:1$7Vqphc5HldCFJVDh$11ebbda2cfdfc56ff8c4327fa6037b3c69a484c442969b27b5962a41414c6923d576fd50bf04edadc086f930d7c48045f1a9dc16709c3c7d2990ebdfbc594e51",
+  "role": "RECRUITER",
+  "department": "Finance",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-11 17:30:50.824055",
+  "updated_at": "2026-06-11 17:30:50.82406",
+  "last_name": "K",
+  "phone_number": "9876543210"
+}, {
+  "user_id": "222b629f-6c83-4557-9753-52f91f6f30fd",
+  "first_name": "Elamathy",
+  "email": "elamathyramanan@gmail.com",
+  "password_hash": "elamathy123",
+  "role": "RECRUITER",
+  "department": "IT Hiring",
+  "account_status": "ACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-05 14:10:11.731031",
+  "updated_at": "2026-06-11 13:08:52.58236",
+  "last_name": "Ramanan",
+  "phone_number": "8148935756"
+}, {
+  "user_id": "21a932de-acb0-4564-974f-e9b3e391eb57",
+  "first_name": "Pooja",
+  "email": "poojarumugam@gmail.com",
+  "password_hash": "pooja123",
+  "role": "RECRUITER",
+  "department": "IT Hiring",
+  "account_status": "INACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-05 14:10:11.731031",
+  "updated_at": "2026-06-11 13:09:14.265524",
+  "last_name": "A",
+  "phone_number": "9842252321"
+}, {
+  "user_id": "41c5e446-a128-4257-8707-1385299feafd",
+  "first_name": "Shagana",
+  "email": "shagana0211@gmail.com",
+  "password_hash": "shagana123",
+  "role": "RECRUITER",
+  "department": "IT Hiring",
+  "account_status": "INACTIVE",
+  "failed_attempt_count": 0,
+  "last_failed_attempt": null,
+  "lock_until": null,
+  "last_login": null,
+  "created_at": "2026-06-05 15:31:37.713328",
+  "updated_at": "2026-06-11 13:17:49.229252",
+  "last_name": "",
+  "phone_number": "9894799149"
+}]
+*/
